524c5f6aa54d47c6bd4b050d645d2e0e8152fac6
rdelazzeri <rdelazzeri@hotmail.com>
2025-05-08 23:32:07 -0300

    docker

--- folios/settings.py
+++ folios/settings.py
@@ -5,16 +5,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=#%m1vx^95ls3fv$^%ffnr7%x3^14vho11)y)6ft=0dwat=gzm'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ 
+DEBUG = bool(os.environ.get("DEBUG", default=0))
+ 
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', v.WelcomeView.as_view(), name = 'welcome'),
     path('folios', v.PortfolioView.as_view(), name = 'portfolio'),
     path('folio/<slug:user>/<slug:slug>', v.FolioView.as_view(), name = 'folio'),
-    path('folio/new', v.folio_new, name = 'folio'),
+    path('folio/new', v.folio_new, name = 'folio_new'),
 
 ]
